917a1e157b691a02f306b18dda39ae84be6c5ef0
Fonti Kar <f.kar@unsw.edu.au>
2023-06-28 16:43:39 +1000

    First draft for package level documentation

---
+++ R/correa-package.R
@@ -0,0 +1,37 @@
+#' Building Darwin Core Archives for the Atlas of Living Australia
+#' 
+#' @description
+#' The Darwin Core Archive (DwCA) is a transferable unit of 
+#' biodiversity data and associated metadata that allows facilities 
+#' like the Atlas of Living Australia (ALA) to efficiently integrate and share 
+#' openly with the community. Creating DwCAs by hand is a notoriously fiddly,
+#' task. 'correa' enables users to efficiently reformat their data to Darwin
+#' Core Standards and generate production-ready DwCA in a reproducible and
+#' transparent workflow.
+#' 
+#' @name correa
+#' @docType package
+#' @references If you have any questions, comments or suggestions, please email
+#' [support@ala.org.au](mailto:support@ala.org.au).
+#'
+#' @keywords internal
+#' @section Functions:
+#' **Detect column names**
+#'
+#'   * [detect_column_names()] 
+#' 
+#' **Build eml.XML**
+#' 
+#'   * [edit_metadata_md()] 
+#'   * [read_metadata_md()] 
+#'   * [md_to_tibble()] 
+#'   * [detect_required_metadata_fields()] 
+#'   * [md_tibble_to_list()] 
+#'   * [update_title()] 
+#'   * [update_names()] 
+
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
