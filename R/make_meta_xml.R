dce5eb1e68fbdfcce6e1ecb28f293c67b34763cc
Fonti Kar <f.kar@unsw.edu.au>
2023-06-29 18:00:41 +1000

    Added function to create core xml

---
+++ R/make_meta_xml.R
@@ -0,0 +1,53 @@
+#' Create core subfield
+#'
+#' @param file_name file name of DwC standard occurrence data
+#' @param field_names colnames of DwC standard occurrence data
+#'
+#' @return xml document 
+#' @export
+#'
+#' @example
+#' make_core_xml()
+make_core_xml <- function(
+    file_name = "occurrence.csv",
+    field_names =  c("basisOfRecord", "eventDate", "decimalLatitude")
+){
+  
+  # Create files sublist
+  files <-  list(
+    location = file_name
+  )
+  
+  # Create id sublist
+  id <- list()
+  
+  # Set id name and attribute
+  attributes(id) <- list(index = "0")
+  
+  # Create field sublists
+  n_fields <- length(field_names)
+  field <- lapply(seq_len(n_fields), function(a){
+    out <- list()
+    attributes(out) <- list(
+      index = as.character(a),
+      url = paste0("http://rs.tdwg.org/dwc/terms/", field_names[[a]]))
+    return(out)
+  })
+  names(field) <- rep("field", n_fields)
+
+  # Join all in same level
+  core <- c(files = list(files), id = list(id), field)
+  names_core <- names(core)
+  
+  # Set attributes of core
+  attributes(core) <- list(encoding="UTF-8",
+                         rowType="http://rs.tdwg.org/dwc/terms/Occurrence",
+                         fieldsTerminatedBy=",",
+                         linesTerminatedBy="\r\n",
+                         fieldsEnclosedBy="&quot;",
+                         ignoreHeaderLines="1")
+  
+  return(set_names(core, names_core))
+}
+
+
