06f5e41c435d91016ce5dd80e137b4375eb48163
Fonti Kar <f.kar@unsw.edu.au>
2023-06-30 11:56:42 +1000

    Removed fieldnames from make_core_xml(), updated README, added test file

--- R/make_meta_xml.R
+++ R/make_meta_xml.R
@@ -1,17 +1,20 @@
 #' Create core subfield
 #'
+#' @param x output of Darwinised occurrence data, output from `detect_dwc_columns()`
 #' @param file_name file name of DwC standard occurrence data
-#' @param field_names colnames of DwC standard occurrence data
 #'
 #' @return xml document 
 #' @export
 #'
-#' @example
-#' make_core_xml()
+#' @examples
+#' DwC_occurrence_data <- tibble(field_names =  c("basisOfRecord", "eventDate", "decimalLatitude"))
+#' make_core_xml(DwC_occurrence_data)
 make_core_xml <- function(
-    file_name = "occurrence.csv",
-    field_names =  c("basisOfRecord", "eventDate", "decimalLatitude")
+    x,
+    file_name = "occurrence.csv"
 ){
+  
+  field_names =  colnames(x)
   
   # Create files sublist
   files <-  list(
@@ -34,20 +37,23 @@ make_core_xml <- function(
     return(out)
   })
   names(field) <- rep("field", n_fields)
-
+  
   # Join all in same level
   core <- c(files = list(files), id = list(id), field)
   names_core <- names(core)
   
   # Set attributes of core
   attributes(core) <- list(encoding="UTF-8",
-                         rowType="http://rs.tdwg.org/dwc/terms/Occurrence",
-                         fieldsTerminatedBy=",",
-                         linesTerminatedBy="\r\n",
-                         fieldsEnclosedBy="&quot;",
-                         ignoreHeaderLines="1")
+                           rowType="http://rs.tdwg.org/dwc/terms/Occurrence",
+                           fieldsTerminatedBy=",",
+                           linesTerminatedBy="\r\n",
+                           fieldsEnclosedBy="&quot;",
+                           ignoreHeaderLines="1")
   
   return(set_names(core, names_core))
+  
+  # TODO: Accept output tibble from `detect_dwc_columns()`
+  # filename = occurrence.csv may be hardcoded, not 100% sure need to check with Data team, if so remove as arg
 }
 
 
