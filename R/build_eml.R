2d90b10ca481a211ed792f02b47a4cbc2885a241
Andrew S <andrew.schwenke@gmail.com>
2023-09-12 11:48:34 +1000

    new eml function

--- R/build_eml.R
+++ R/build_eml.R
@@ -14,200 +14,57 @@
 # citation - to ensure your dataset gets cited the way you want
 
 #' @param path file path where metadata template will be copied to
-#'
+#' @param type type of template (we can support more)
 #' @return template file opened in RStudio for user to edit
 #' @export
 #' @importFrom usethis edit_file
-#'
-#' @examples
-#' \dontrun{
-#' edit_metadata_md()
-#' }
-template_open <- function(path = ".") {
+template_edit <- function(path = ".", type = "ALA") {
   # Template stored in inst/markdown/user_template.md
-  path_to_md_template <- system.file("markdown/user_template.md", package = "galaxias")
+  # Switch if ALA then
+  path_to_md_template <- switch(type,
+    "ALA" = {
+      path_to_md_template <- system.file(
+        "markdown/user_template.md",
+        package = "galaxias"
+      )
+    }
+  )
 
   # Copy the template to root of directory
   file.copy(path_to_md_template, ".")
 
   # Edit in RStudio
   edit_file(paste0(path, "/user_template.md"))
-
-  # TODO:
-  # Need a mechanism to rename the copied file, perhaps a menu() prompt and wraps around file.rename()
-  # Checks if its existing, if so DO NOT overwrite
-  # AS: See my shiny app for ideas
 }
 
-#' Read metadata md information into R
-#'
-#' @param path file path pointing .md metadata template
-#'
-#' @return tibble containing concatenated strings from each field
-#' @keywords internal
-#' @examples
-#' path_to_md_template <- system.file("markdown/westerband_template.md", package = "galaxias")
-#' metadata_ls <- read_metadata_md(path_to_md_template)
-#'
-template_read <- function(path) {
-  scan_output <- scan(path,
-    what = "character",
-    sep = "\n",
-    blank.lines.skip = FALSE
-  )
-
-  return(scan_output)
-}
-
-# Subfunctions to convert markdown into a tibble
-
-#' Read metadata md information into R
-#'
-#' @param path file path pointing .md metadata template
-#'
-#' @return tibble containing concatenated strings from each field
-#' @keywords internal
-#' @examples
-#' path_to_md_template <- system.file("markdown/westerband_template.md", package = "galaxias")
-#' metadata_ls <- read_metadata_md(path_to_md_template)
-#'
-read_metadata_md <- function(path) {
-  scan_output <- scan(path,
-    what = "character",
-    sep = "\n",
-    blank.lines.skip = FALSE
-  )
-
-  return(scan_output)
-}
-
-#' Extract metadata text and wrangle into tibble
-#'
-#' @param scan_output output of `read_metadata_md()`
-#'
-#' @return tibble where text from each field is concatenated by header
-#' @importFrom dplyr tibble mutate slice filter pull select
-#' @importFrom stringr str_detect regex str_replace
-#' @importFrom tidyr nest unnest
-#' @importFrom purrr map
 #' @export
-#' @examples
-#' scan_output <- read_metadata_md("doc/westerband_template.md")
-#' extract_md_contents(scan_output)
-template_clean <- function(scan_output) {
-  grouped_by_header <- tibble(input = scan_output) |>
-    mutate(group_id = str_detect(input, regex("^#")) |> cumsum())
-
-  # A function to concatenate text in each field
-  join_text <- function(grouped_by_header) {
-    grouped_by_header |>
-      slice(2:nrow(grouped_by_header)) |> # Take anything below the 1st row
-      filter(!input == "") |> # Exclude the empty elements of input
-      pull(input) |> # Isolate column as vector
-      paste(collapse = "") #  Join strings
-
-    # TODO: Need to replace double/multiple spaces with single space for nicer
-    # output
-  }
-
-  # Mapping across the group_id
-  nested_grouped_by_header <- grouped_by_header |>
-    nest(text = input) |>
-    mutate(joined_str = map(
-      text,
-      ~ join_text(.x)
-    ))
-
-  # A function to extract the header from the ## notation
-  extract_clean_header <- function(grouped_by_header) {
-    grouped_by_header |>
-      slice(1) |>
-      pull(input) |>
-      trimws("both") |>
-      str_replace(pattern = "^#{1,}", replacement = "") |> # Replace any number of # with nothing
-      str_replace(pattern = "\\+$|\\*$", replacement = "") |> # Replace + and * with nothing
-      trimws("both")
-  }
-
-  # Create header variable to denote the section the text is from
-  metadata_tibble <- nested_grouped_by_header |>
-    mutate(header = map(
-      text,
-      ~ extract_clean_header(.x)
-    )) |>
-    select(group_id, header, joined_str, text) |>
-    unnest(cols = c(header, joined_str)) |>
-    print(n = nrow(nested_grouped_by_header))
-
-  return(metadata_tibble)
-}
-
-#' Convert metadata tibble into list
-#'
-#' @param metadata_tibble output from `md_to_tibble()`
-#'
-#' @return named list with field name as name
-#' @importFrom dplyr select
-#' @importFrom purrr map list_transpose set_names
-#' @export
-#' @examples
-#' path_to_md_template <- system.file("markdown/westerband_template.md", package = "galaxias")
-#' scan_output <- read_metadata_md(path_to_md_template)
-#' metadata_tibble <- extract_md_contents(scan_output)
-#' metadata_ls <- metadata_tibble |> md_tibble_to_list()
-#'
-make_list <- function(metadata_tibble) {
-  # Convert tibble to list
-  metadata_list <- metadata_tibble |>
-    select(joined_str) |> # Select the text column
-    as.list() |> # Treat as list
-    list_transpose()
-
-  # Remove join_str
-  cleaned_metadata_list <- map(
-    metadata_list,
-    ~ unname(.x)
+function_thing <- function(template, output = NULL) {
+  rmarkdown::pandoc_convert(normalizePath(template),
+    to = "docbook",
+    output = paste0(tempdir(), "/test.xml"),
+    options = "-s"
   )
+  docbook <- xml2::read_xml(paste0(tempdir(), "/test.xml"))
+  docbook <- xml2::xml_ns_strip(docbook)
+  sections <- xml_find_all(docbook, "//section", flatten = FALSE)
+  result_list <- lapply(sections, function(sec) {
+    title <- xml_text(xml_find_first(sec, "./title"))
+    para <- xml_text(xml_find_all(sec, "./para"))
+    list(title = title, para = para)
+  })
+  xml_ids <- sapply(sections, function(sec) xml_attr(sec, "id"))
+  names(result_list) <- xml_ids
 
-  # Set names to match the template
-  set_names(cleaned_metadata_list, metadata_tibble$header)
+  my_eml <- EML::eml$eml(
+    dataset = EML::eml$dataset(
+      result_list$fish$title,
+      methods = result_list$methods$para,
+      creator = result_list$creator$para,
+      contact = result_list$creator$para
+    )
+  )
+  message("Writing EML to file...\n")
+  write_eml(my_eml, "./eml_test.xml")
 }
 
-#' Extract first or last name from Name field
-#'
-#' @param list output from `convert_md_tibble_to_list()`
-#' @param var character, name field (Name/Creator)
-#' @param which character denoting which part of name to extract (first/last)
-#'
-#' @return vector of length 1 containing first or last name
-#' @keywords internal
-#' @importFrom stringr word
-#' @importFrom purrr pluck
-#' @examples
-#' path_to_md_template <- system.file("markdown/westerband_template.md",
-#'   package = "galaxias"
-#' )
-#' scan_output <- read_metadata_md(path_to_md_template)
-#' metadata_tibble <- extract_md_contents(scan_output)
-#' convert_md_tibble_to_list(metadata_tibble) |> extract_name_from_ls(
-#'   which =
-#'     "last"
-#' )
-#'
-extract_name_from_ls <- function(list, var = "Name", which) {
-  if (which == "first") {
-    name <- list |>
-      pluck(var) |>
-      word(1)
-  }
-
-  if (which == "last") {
-    name <- list |>
-      pluck(var) |>
-      word(2)
-  }
-
-  return(name)
-
-  # TODO: Option to take name from Creator if Name is not supplied, option to take first full name if multiple names are supplied
-}
+# Define constants for licence you can pick 1, 2, 3
