083ee43ffc31fc182c6b600c4e264c99dcc0b4cb
Andrew S <andrew.schwenke@gmail.com>
2023-09-18 14:14:29 +1000

    clean up build eml etc

--- R/build_eml.R
+++ R/build_eml.R
@@ -1,26 +1,18 @@
-#' Functions to open metadata markdown templates, and read markdown
-#' into R for use.
-#' Required metadata fields
-# title
-# description
-# publishing organization
-# type
-# license
-# contact(s)
-# creator(s)
-# metadata provider(s)
-# Recommended metadata fields
-# sampling methodology - in situations where data comes from a sampling event
-# citation - to ensure your dataset gets cited the way you want
-
-#' @param path file path where metadata template will be copied to
-#' @param type type of template (we can support more)
-#' @return template file opened in RStudio for user to edit
+#' Open a markdown template file for creating EML metadata
+#' @param path File path where metadata template will be copied to
+#' @param type Which template flavour to open (default: "ALA", more to come)
+#' @param licence Numeric value corresponding to a valid licence to use for the
+#' dataset. Read more [here:](https://support.ala.org.au/support/solutions/articles/6000197133-what-licence-should-i-use-)
+#' #' \itemize{
+#'   \item 1 = Creative Commons Zero - CC0
+#'   \item 2 = Attribution – CC BY (4.0)
+#'   \item 3 = Attribution-Noncommercial – CC BY-NC
+#' @return Template file opened in RStudio for user to edit
 #' @export
 #' @importFrom usethis edit_file
-template_edit <- function(path = ".", type = "ALA") {
-  # Template stored in inst/markdown/user_template.md
-  # Switch if ALA then
+edit_template <- function(path = ".",
+                          type = "ALA",
+                          licence = 1) {
   path_to_md_template <- switch(type,
     "ALA" = {
       path_to_md_template <- system.file(
@@ -29,20 +21,30 @@ template_edit <- function(path = ".", type = "ALA") {
       )
     }
   )
-
-  # Copy the template to root of directory
   file.copy(path_to_md_template, ".")
-
-  # Edit in RStudio
   edit_file(paste0(path, "/user_template.md"))
 }
 
-# template <- "./inst/markdown/westerband_template_modified.md"
-#' @export
-function_thing <- function(template, output_dir = NULL) {
-  # docbook xml only needed temporarily
-  # doc_output <- paste0(tempdir(), "/test.xml")
 
+#' Build and export an EML file using an input markdown template
+#' Reference: EML package function `set_TextType()`
+#' @param template Path to input markdown template
+#' @param output_dir Path to output directory to save EML file. Defaults to
+#' current working directly ("./eml.xml")
+#' @param validate_eml Logical; whether to validate the EML file using the EML
+#' package function `eml_validate()`
+#' @return EML written to output directory
+#' @export
+#' @examples
+#' build_eml(template = "./user_template.md")
+#' build_eml(
+#'   template = "./user_template.md",
+#'   output_dir = "./metadata/my_eml.xml",
+#'   validate = TRUE
+#' )
+build_eml <- function(template,
+                      output_dir = "./eml.xml",
+                      validate = FALSE) {
   rmarkdown::pandoc_convert(normalizePath(template),
     to = "docbook",
     output = paste0(tempdir(), "/temp.xml"),
@@ -59,25 +61,7 @@ function_thing <- function(template, output_dir = NULL) {
   xml_ids <- sapply(sections, function(sec) xml2::xml_attr(sec, "id"))
   names(result_list) <- xml_ids
 
-  # This is where the EML is built, by extracting the relevant information from
-  # the result_list, and assinging to the appropriate EML fields
-  # TODO: This will be factored out, because it could potentially get quite
-  # large? Unsure.
-
-  # I'm going to try write them in the order that they appear in the template
-  # my_eml <- EML::eml$eml(
-  #   dataset = EML::eml$dataset(
-  #     result_list$fish$title,
-  #     methods = result_list$methods$para,
-  #     creator = result_list$creator$para,
-  #     contact = result_list$creator$para
-  #   )
-  # )
-
-  # Note - the fields are accessed in the result list, first by their markdown
-  # tag
-  # {#tag}, then by the paragraph element typically
-  # methods and projects fields look extensible
+  # Assign markdown tags to relevant EML fields
   my_eml <- EML::eml$eml(
     packageId = uuid::UUIDgenerate(),
     additionalMetadata = NULL,
@@ -90,7 +74,6 @@ function_thing <- function(template, output_dir = NULL) {
       # ?? == technical-description
       # ?? == data-quality-description
       # methods == methods-description
-
       methods = list(
         sampling = result_list$methods$para,
         description = result_list$`data-quality-description`$para,
@@ -99,7 +82,7 @@ function_thing <- function(template, output_dir = NULL) {
       purpose = result_list$purpose$para,
       # data-generalisations
       # information-withheld
-      # This is different to the reference of the actual dataset
+      # Note: this is different to the reference of the actual dataset
       referencePublication = result_list$citation$para,
       intellectualRights = result_list$license$para,
       licensed = result_list$rights$para,
@@ -107,28 +90,52 @@ function_thing <- function(template, output_dir = NULL) {
       contact = result_list$creator$para
       # electronicMailAddress
       # individualName
-      # project field could be used?
+      # ?? == institutions
+      # (Primary contacts)
+      # ?? == primary-contact-name
+      # ?? == primary-contact-email
+      # additionalMetadata
     )
   )
-
-  # ?? == institutions
-  # (Primary contacts)
-  # ?? == primary-contact-name
-  # ?? == primary-contact-email
-
-  # additionalMetadata
-
+  if (validate) {
+    check_eml(my_eml)
+  }
+  validate_output_path(output_dir)
   message("Writing EML to file...\n")
-  # TODO: I want to be able to specify the output directory and file name
-  EML::write_eml(my_eml, "./eml_test.xml")
+  EML::write_eml(my_eml, output_dir)
 }
 
-# Define constants for licence you can pick 1, 2, 3
-# Hardcoding rights:
-# intellectualRights = EML::eml$intellectualRights(
-# EML::eml$para("This work is licensed under a Creative Commons Attribution
-# 4.0 International License.")
+#' Validate a user input output path / file name
+#' @param filepath path to output file
+#' @return NULL - console output. Stops if validation fails.
+#' @keywords internal
+validate_output_path <- function(filepath) {
+  # Extract the directory from the filepath
+  directory_name <- dirname(filepath)
 
+  # Check if directory exists
+  if (!dir.exists(directory_name)) {
+    stop("\033[31mThe specified directory does not exist!\033[0m",
+      call. = FALSE
+    )
+  }
+
+  # Check if directory is writable
+  if (file.access(directory_name, mode = 2) != 0) {
+    stop("\033[31mThe specified directory is not writable!\033[0m",
+      call. = FALSE
+    )
+  }
+
+  # Check if file already exists (optional)
+  if (file.exists(filepath)) {
+    stop("\033[31mA file with the specified name already exists!\033[0m",
+      call. = FALSE
+    )
+  }
+}
+
+# Notes and stuff
 # Add the your text here stuff for the template
 # UUIDgenerate for the unique identifier - put in the function call, if NULL,
 # user has to supply one.
@@ -140,4 +147,6 @@ function_thing <- function(template, output_dir = NULL) {
 # is useful to the user - these can be indicated as italics or something so they
 # know it won't go into the template.
 # test multiple contact parsing
-EML::eml_validate("./eml_test.xml")
+# Could also return the EML list object? would only make sense if also provide a
+# function to input the list and then export EML. Easy to do.
+# Check the UUIDgenerate is working
--- R/check_eml.R
+++ R/check_eml.R
@@ -1,19 +1,42 @@
-#' Detect required fields in metadata template
-#'
-#' @param metadata_tibble
+#' Check for required ALA fields in an EML list
+#' @param eml_list
+#' @return NULL - console output
 
-detect_required_metadata <- function(metadata_tibble) {
-  required_md_fields <- c(
-    "Title", "Public Short Description",
-    "Citation", "Creator", "License"
-  )
-
-  # THIS DOES NOT WORK
-
-  required_md_data <- md_tibble |>
-    filter(header %in% required_md_fields)
-
-  if (any(required_md_data$joined_str == "")) {
-    # TODO: Dax's cool detect columns and report percentage code
+check_eml <- function(eml_list) {
+  message("Checking required fields...\n")
+  present_count <- 0
+  missing_fields <- c()
+  silently <- lapply(required_fields, function(x) {
+    if (length(eml_list$dataset[[x]]) == 0) {
+      missing_fields <<- c(missing_fields, x)
+      message(paste0(x, " is missing\n"))
+    } else {
+      message(paste0(x, " is present\n"))
+      present_count <<- present_count + 1
+    }
+  })
+  # Calculate the percentage of fields that are present
+  percentage_present <- (present_count / length(required_fields)) * 100
+  # Display the percentage
+  if (percentage_present < 100) {
+    formatted_missing_fields <- paste0("- ", missing_fields, collapse = "\n")
+    cat(paste0(
+      "\033[31mWarning:\033[0m ",
+      percentage_present,
+      "% of required fields present.\nMissing fields:\n",
+      formatted_missing_fields, "\n"
+    ))
+  } else {
+    message("\033[32mSuccess:\033[0m 100% of fields present.")
   }
 }
+
+# Vector of field terms required by ALA
+required_fields <- c(
+  "title",
+  "abstract",
+  # not sure if referencePublication is equivalent to "citation" in ALA terms
+  "referencePublication",
+  "creator",
+  "intellectualRights"
+)
