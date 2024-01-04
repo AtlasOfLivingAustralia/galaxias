4e0c6643895323f66b2ea20470dcc894f869c59c
Martin Westgate <martinjwestgate@gmail.com>
2024-01-04 16:38:28 +1100

    more information-rich UI for `map_terms()`

--- R/get_terms.R
+++ R/get_terms.R
@@ -1,6 +1,8 @@
 #' get DwC terms and format them
+#' @importFrom dplyr bind_rows
 #' @importFrom dplyr filter
 #' @importFrom dplyr select
+#' @importFrom purrr pluck
 #' @importFrom tawnydragon tdwg
 #' @importFrom tawnydragon tdwg_standards
 #' @importFrom tawnydragon tdwg_terms
@@ -8,19 +10,26 @@
 #' @noRd
 #' @keywords Internal
 get_terms <- function(){
-  terms <- tdwg() |>
+  terms_full <- tdwg() |>
     tdwg_standards(label == "Darwin Core",
                    status == "recommended") |>
     tdwg_terms() |>
-    summarize()
+    pluck("terms")
   
-  parents <- terms |>
+  parents <- terms_full |>
     filter(is.na(parent_class) & 
              code %in% parent_class) |>
-    select(-parent_class, -status)
+    select("code", "description")
   
-  terms <- terms |>
-    filter(!is.na(parent_class))  
+  terms <- terms_full |>
+    filter(!(code %in% parents$code),
+           type == "term") |>
+    select("parent_class", "code", "label", "description", "examples")
+  terms$parent_class[is.na(terms$parent_class)] <- "No parent class"
+  
+  parents <- bind_rows(parents, 
+                       tibble(code = "No parent class", 
+                              description = "None given"))
   
   list(parents = parents, terms = terms)
 }
--- R/map_terms.R
+++ R/map_terms.R
@@ -24,47 +24,45 @@ map_terms_ui <- function(df){
     title = "galaxias::map_terms",
     theme = bs_theme(bootswatch = "minty"),
     layout_columns(
-      col_widths = c(4, 8),
+      col_widths = c(8, 4),
       left_col(),
       right_col()
     )
   )
 }
 
-#' left column ui
-#' @importFrom htmltools br
+#' right column ui
+#' @importFrom htmltools div
 #' @importFrom htmltools h4
-#' @importFrom shiny actionButton
-#' @importFrom shiny verbatimTextOutput
 #' @noRd
 #' @keywords Internal
 left_col <- function(){
   div(
-    h4("Supplied column names"),
-    colnames_div(df),
-    detect_colnames(df),
-    br(),
-    h4("tracking"),
-    verbatimTextOutput("x"), # testing only
-    actionButton(
-      inputId = "exit",
-      label = "Save & Exit"
-    )
+    h4("Darwin Core terms"),
+    terms_list()
   )
 }
 
-#' right column ui
+#' left column ui
+#' @importFrom dplyr pull
 #' @importFrom htmltools br
 #' @importFrom htmltools h4
-#' @importFrom shiny textOutput
-#' @importFrom shiny uiOutput
+#' @importFrom purrr pluck
+#' @importFrom shiny actionButton
 #' @noRd
 #' @keywords Internal
 right_col <- function(){
   div(
-    h4("Darwin Core terms"),
+    h4("Supplied terms"),
+    colnames_div(df),
+    detect_colnames(df),
     br(),
-    terms_list(),
+    h4("tracking"),
+    # verbatimTextOutput("x"), # testing only
+    actionButton(
+      inputId = "exit",
+      label = "Save & Exit"
+    ),
     move_term_to_colname(group = {get_terms() |> 
         pluck("parents") |> 
         pull("code")}),
@@ -73,12 +71,11 @@ right_col <- function(){
 }
 
 #' server function for map_terms()
-#' @importFrom dplyr filter
-#' @importFrom dplyr pull
-#' @importFrom purrr pluck
-#' @importFrom shiny renderPrint
-#' @importFrom shiny selectInput
+#' @importFrom shiny observe
+#' @importFrom shiny observeEvent
+#' @importFrom shiny reactiveValues
 #' @importFrom shiny stopApp
+#' @importFrom stringr str_extract
 #' @noRd
 #' @keywords Internal
 map_terms_server <- function(input, output) {
@@ -98,12 +95,14 @@ map_terms_server <- function(input, output) {
     lapply(
       seq_len(ncol(df)),
       function(a){
-        check_input <- input[[paste0("sort_", a)]]
+        check_input <- input[[paste0("sort_", a)]] |>
+          str_extract("^[:alpha:]+\\n") |>
+          sub("\\n$", "", x = _)
         if(!is.null(check_input)){
           if(length(check_input) < 1){
             mapping$result[[a]] <- NA  
           }else{
-            mapping$result[[a]] <- check_input    
+            mapping$result[[a]] <- check_input 
           }
         }else{
           mapping$result[[a]] <- NA
@@ -118,7 +117,7 @@ map_terms_server <- function(input, output) {
   })
 
   ## for testing purposes only, track internal objects in the browser
-  output$x <- renderPrint({str(mapping$result)})
+  # output$x <- renderPrint({str(mapping$result)})
 }
 
 #' Internal function to add 
@@ -142,8 +141,11 @@ colnames_div <- function(df){
     )
 }
 
+#' build a list of terms with navbar
 #' @importFrom bslib navset_pill_list
 #' @importFrom bslib nav_panel
+#' @importFrom dplyr filter
+#' @importFrom purrr pluck
 #' @noRd
 #' @keywords Internal
 terms_list <- function(){
@@ -165,17 +167,10 @@ terms_list <- function(){
   )
 }
 
-
-#' Internal function to convert terms to divs
-#' @importFrom bslib card
-#' @importFrom bslib card_body
-#' @importFrom bslib card_footer
-#' @importFrom bslib card_header
-#' @importFrom dplyr filter
+#' Internal function to convert terms to boxes
 #' @importFrom htmltools div
-#' @importFrom htmltools tag
 #' @importFrom purrr map
-#' @importFrom purrr pluck
+#' @importFrom shiny markdown
 #' @noRd
 #' @keywords Internal
 terms_div <- function(terms, group){
@@ -184,15 +179,51 @@ terms_div <- function(terms, group){
     id = paste0("sort_terms_", group),
     map(.x = seq_len(nrow(terms)),
            .f = \(x){
-             card(class = "terms",
-                  card_header(terms$code[x]),
-                  card_body(terms$description[x]))
-                  # card_footer(terms$example[x]) # not yet available
+             custom_value_box(
+               term = terms$code[x],
+               markdown(paste0("*", terms$description[x], "*")),
+               markdown(paste0("**Example:** ", 
+                               strsplit(terms$examples[x], ";")[[1]][1]
+               )),
+               theme = "success"
+             )
+             # card(
+             #      class = "terms",
+             #      card_body(terms$code[x], class = "value-box-title"),
+             #      shiny::markdown(terms$description[x])
+             #      # title = terms$description[x],
+             #      # value = terms$code[x]
+             #      # card_footer(terms$examples[x]))
+             # ) 
            })
   )
 }
 
+#' Internal function to build own boxes - heavily based on `value_box()` code
+#' @importFrom bslib card
+#' @importFrom bslib as_fill_carrier
+#' @importFrom bslib value_box_theme
+#' @importFrom htmltools css
+#' @importFrom htmltools div
+#' @importFrom htmltools p
+#' @noRd
+#' @keywords Internal
+custom_value_box <- function(term, ..., theme){
+  dots <- list(...)
+  term <- p(term, class = "value-box-value")
+  contents <- div(class = "value-box-area", term, !!!dots) |>
+    as_fill_carrier()
+  theme <- value_box_theme(theme)
+  card(class = c("bslib-value-box", theme$class),
+       style = css(color = theme$fg, 
+                   background_color = theme$bg, 
+                   `--bslib-color-fg` = theme$fg, 
+                   `--bslib-color-bg` = theme$bg),
+       contents)
+}
+
 #' Internal function to quantify which terms are added to each colname
+#' @importFrom purrr map
 #' @importFrom sortable sortable_js
 #' @importFrom sortable sortable_js_capture_input
 #' @importFrom sortable sortable_options
@@ -213,6 +244,7 @@ detect_colnames <- function(df){
 }
 
 #' JS support for draggable elements
+#' @importFrom purrr map
 #' @importFrom sortable sortable_js
 #' @importFrom sortable sortable_options
 #' @noRd
@@ -236,9 +268,9 @@ move_term_to_colname <- function(group){
 
 #' Support dropping divs into a new table
 #' @importFrom htmlwidgets JS
+#' @importFrom purrr map
 #' @importFrom sortable sortable_js
 #' @importFrom sortable sortable_options
-#' @importFrom purrr map
 #' @noRd
 #' @keywords Internal
 remove_terms_from_list <- function(df){
