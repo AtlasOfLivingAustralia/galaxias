083b5283163283abd42420bfb64f322c2fb42e9e
Martin Westgate <martinjwestgate@gmail.com>
2024-05-09 16:21:16 +1000

    update help files to show accepted values for `basisOfRecord`

--- R/use_basisOfRecord.R
+++ R/use_basisOfRecord.R
@@ -3,8 +3,10 @@
 #' basisOfRecord is a standard - often required - field in the Darwin Core 
 #' Standard, which describes broadly how the data were collected.
 #' @param df a `data.frame` or `tibble` that the column should be appended to.
-#' @param value what value should this field take? Only accepts camelCase, for
-#' consistency with field names.
+#' @param value what value should this field take? Should be one of 
+#' `"humanObservation"`, `"machineObservation"`, `"livingSpecimen"`, 
+#' `"preservedSpecimen"`, `"fossilSpecimen"` or `"materialCitation"`. Only accepts 
+#' camelCase, for consistency with field names.
 #' @returns A tibble with an attached `basisOfRecord` field
 #' @details
 #' Currently only accepts a single argument. Could probably be made more 
