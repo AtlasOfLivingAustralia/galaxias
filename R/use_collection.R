1780385a63551282ab51d7f4b2a60a7d1a9d542f
Dax Kellie <dax.kellie@csiro.au>
2024-07-02 17:16:54 +1000

    Update mutate syntax, add basic checks to `use_collection()`

--- R/use_collection.R
+++ R/use_collection.R
@@ -1,11 +1,13 @@
 #' Add museum- or collection-specific information to a `tibble`
 #' 
+#' @description
 #' Format fields that specify the collection or catelog number of a 
 #' specimen or occurrence record.
 #' 
 #' In practice this is no different from using `mutate()`, but gives some 
 #' informative errors, and serves as a useful lookup for fields in 
 #' the Darwin Core Standard.
+#' 
 #' @param df a `data.frame` or `tibble` that the column should be appended to.
 #' @param datasetID An identifier for the set of data. May be a global unique 
 #' identifier or an identifier specific to a collection or institution.
@@ -36,20 +38,67 @@
 #' @importFrom rlang abort
 #' @export
 use_collection <- function(
-    df,
+    .df,
     datasetID = NULL,
     datasetName = NULL,
     catalogNumber = NULL,
     .keep = "unused"
 ){
-  if(missing(df)){
+  if(missing(.df)){
     abort("df is missing, with no default")
   }
-  result <- df |>
-    mutate(datasetID = {{datasetID}},
-           datasetName = {{datasetName}},
-           catalogNumber = {{catalogNumber}},
+  fn_args <- ls()
+  check_missing_all_args(match.call(), fn_args)
+  
+  # capture arguments as a list of quosures
+  # NOTE: enquos() must be listed alphabetically
+  fn_quos <- enquos(catalogNumber, datasetID, datasetName)
+  names(fn_quos) <- fn_args
+  
+  # find arguments that are NULL but exist already in `df`
+  # these DwC columns are otherwise deleted by `mutate()` later
+  fn_quo_is_null <- fn_quos |> 
+    purrr::map(\(user_arg)
+               rlang::quo_is_null(user_arg)) |> 
+    unlist()
+  
+  null_col_exists_in_df <- fn_quo_is_null & (names(fn_quos) %in% colnames(.df))
+  
+  if(any(null_col_exists_in_df)){
+    purrr::pluck(fn_quos, names(which(null_col_exists_in_df))) <- rlang::zap()
+  }
+  
+  # Update df
+  result <- .df |> 
+    mutate(!!!fn_quos, 
            .keep = .keep)
   
-  result
+  check_datasetName(result, level = "abort")
+  
+  # NOTE: Unsure of what checks are useful for columns below
+  # check_catalogNumber(result, level = "abort")
+  # check_datasetID(result, level = "abort")
+  
+  return(result)
 }
+
+
+#' Check datasetName field is valid
+#' 
+#' @rdname check_dwc
+#' @param level what action should the function take for non-conformance? 
+#' Defaults to `"inform"`.
+#' @order 7
+#' @export
+check_datasetName <- function(.df, 
+                              level = c("inform", "warn", "abort")
+){
+  level <- match.arg(level)
+  if(any(colnames(.df) == "datasetName")){
+    .df |>
+      select("datasetName") |>
+      check_is_string(level = level)
+  }
+  .df
+}
+
