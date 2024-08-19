86e9eb312397fee022a4dabc03c483eb29536437
Dax Kellie <dax.kellie@csiro.au>
2024-08-19 18:16:41 +1000

    Add more `use_scientific_name()` tests

--- tests/testthat/test-use_scientific_name.R
+++ tests/testthat/test-use_scientific_name.R
@@ -19,7 +19,7 @@ test_that("use_scientific_name returns tibble with updated dwc column names", {
     quiet_use_scientific_name(scientificName = user_col)
   
   expect_s3_class(result$result, c("tbl_df", "tbl", "data.frame"))
-  expect_match(colnames(result$result), c("scientificName"))
+  expect_equal(colnames(result$result), c("scientificName"))
 })
 
 test_that("use_scientific_name detects unnamed but existing dwc column names in df", {
@@ -44,4 +44,55 @@ test_that("use_scientific_name has progress messages", {
   
   expect_false(is.null(result$messages))
   
+})
+
+test_that("use_scientific_name checks scientificName format", {
+  quiet_use_scientific_name <- purrr::quietly(use_scientific_name)
+  df <- tibble(scientificName = c("Callocephalon fimbriatum", "Eolophus roseicapilla"),
+               col2 = 1:2)
+  
+  result <- df |> quiet_use_scientific_name()
+  
+  expect_s3_class(result$result, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(colnames(result$result), c("scientificName", "col2"))
+  
+  expect_error(suppressMessages(
+    df |> use_scientific_name(scientificName = col2)
+  ),
+  "scientificName must be a character vector, not integer"
+  )
+})
+
+test_that("use_scientific_name checks scientificNameRank format", {
+  quiet_use_scientific_name <- purrr::quietly(use_scientific_name)
+  df <- tibble(scientificNameRank = c("family", "species"),
+               col2 = 1:2)
+  
+  result <- df |> quiet_use_scientific_name()
+  
+  expect_s3_class(result$result, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(colnames(result$result), c("scientificNameRank", "col2"))
+  
+  expect_error(suppressMessages(
+    df |> use_scientific_name(scientificNameRank = col2)
+  ),
+  "scientificNameRank must be a character vector, not integer"
+  )
+})
+
+test_that("use_scientific_name checks scientificNameAuthorship format", {
+  quiet_use_scientific_name <- purrr::quietly(use_scientific_name)
+  df <- tibble(scientificNameAuthorship = c("(Györfi, 1952)", "R. A. Graham"),
+               col2 = 1:2)
+  
+  result <- df |> quiet_use_scientific_name()
+  
+  expect_s3_class(result$result, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(colnames(result$result), c("scientificNameAuthorship", "col2"))
+  
+  expect_error(suppressMessages(
+    df |> use_scientific_name(scientificNameAuthorship = col2)
+  ),
+  "scientificNameAuthorship must be a character vector, not integer"
+  )
 })
