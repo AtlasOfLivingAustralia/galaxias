34414ce8048aa5e0fd8047e99de0796e6f17105f
Dax Kellie <dax.kellie@csiro.au>
2025-04-24 16:52:29 +1000

    Merge to one single `use_data()` function
    * Keeps things simple and echoes `use_metadata()`

--- R/use_data.R
+++ R/use_data.R
@@ -1,7 +1,7 @@
-#' Save standardised occurrence-type data
+#' Use standardised data in a Darwin Core Archive
 #' 
 #' @description
-#' Once data conform to Darwin Core Standard, `use_data_occurrences()` makes it 
+#' Once data conform to Darwin Core Standard, `use_data()` makes it 
 #' easy to save data in the correct place for building a Darwin Core Archive 
 #' with [build_archive()].
 #' @details
@@ -9,71 +9,33 @@
 #' folder that `galaxias` uses to build a Darwin Core Archive using 
 #' [build_archive()].
 #' @param ... (string) Unquoted name of `tibble`/`data.frame` to save.
-#' @param overwrite (logical) By default, `use_data_occurrences()` will not 
+#' @param type (string) Data type. Designates whether data are saved as 
+#'   `occurrences.csv` or `events.csv`. `type` is determined by detecting 
+#'   Event-type column names in data (only valid in an Event-type dataset).
+#' @param overwrite (logical) By default, `use_data()` will not 
 #'   overwrite existing files. If you really want to do so, set this to `TRUE`. 
 #' @return Invisibly returns the location of the saved csv file.
-#' @seealso [use_data_events()]
+#' @seealso [use_metadata()]
 #' @export
-use_data_occurrences <- function(...,
-                                 overwrite = FALSE) {
+use_data <- function(...,
+                     type = c("occurrence", "event"),
+                     overwrite = FALSE) {
   
   obj <- get_objs_from_dots(dots(...))
   user_data <- check_is_data(obj)
   
   # Check if data is dwc compliant here?
   
-  # There should be some kind of `check_is_occ_based()` function to make sure 
-  #   users are supplying the correct data for this operation.
+  type <- check_data_type(user_data)
   
-  usethis::use_directory("data-publish")
-  file_path <- fs::path("data-publish", "occurrences.csv")
+  usethis::use_directory("data-publish") # add folder if it's not already there
   
-  if(file.exists(file_path)){
-    if(overwrite){
-      cli::cli_progress_step("Overwriting existing file `{.file {file_path}}`.")        
-      readr::write_csv(user_data, file = file_path)
-      cli::cli_progress_done()
-    }else{
-      c("{.file {file_path}} already exists.",
-        i = "Set `overwrite = TRUE` to overwrite existing file.") |>
-        cli::cli_inform()     
-    }
-  }else{
-    cli::cli_progress_step("Writing {.file {file_path}}.")
-    readr::write_csv(user_data, file = file_path)
-    cli::cli_progress_done()
+  if(type == "occurrences") {
+    file_path <- fs::path("data-publish", "occurrences.csv")
+  } else {
+    file_path <- fs::path("data-publish", "events.csv")
   }
   
-}
-
-#' Save standardised event-type data
-#' 
-#' @description
-#' Once data conform to Darwin Core Standard, `use_data_events()` makes it 
-#' easy to save data in the correct place for building a Darwin Core Archive 
-#' with [build_archive()].
-#' @details
-#' This function saves data in the `data-publish` folder, which is the default 
-#' folder that `galaxias` uses to build a Darwin Core Archive using 
-#' [build_archive()].
-#' @param ... (string) Unquoted name of `tibble`/`data.frame` to save.
-#' @param overwrite (logical) By default, `use_data_events()` will not 
-#'   overwrite existing files. If you really want to do so, set this to `TRUE`. 
-#' @return Invisibly returns the location of the saved csv file.
-#' @seealso [use_data_occurrences()]
-#' @export
-use_data_events <- function(...,
-                            overwrite = FALSE) {
-  
-  obj <- get_objs_from_dots(dots(...))
-  user_data <- check_is_data(obj)
-  
-  # Check if data is dwc compliant here?
-  
-  # `check_is_events_based()`?
-  
-  usethis::use_directory("data-publish")
-  file_path <- fs::path("data-publish", "events.csv")
   
   if(file.exists(file_path)){
     if(overwrite){
@@ -124,5 +86,23 @@ check_is_data <- function(obj, error_call = rlang::caller_env()) {
   }
   
   obj
+  
+}
+
+
+check_data_type <- function(data, error_call = rlang::caller_env()) {
+  
+  if (any(colnames(data) %in% c("eventID", "parentEventID"))) {
+    type <- "event"
+    not <- "occurrence"
+  } else {
+    type <- "occurrence"
+    not <- "event"
+  }
+  bullets <- c("Data identified as {type}-type.",
+               i = cli::col_grey("If this is incorrect, use {.code use_date(type = \"{not}\")} instead."))
+  cli::cli_inform(bullets)
+  
+  return(type)
   
 }
