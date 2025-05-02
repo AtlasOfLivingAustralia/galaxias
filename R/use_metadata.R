9aefcf5fc732cea6c20fc5dea6b688b76bff3574
Martin Westgate <martinjwestgate@gmail.com>
2025-05-02 15:44:16 +1000

    update `use_metadata()`
    - add tests
    - remove requirement to specify `data-publish` in `destination` field, for consistency with `use_data()`
    - add new function `use_directory()` to ensure consistent behaviour between users and testthat

--- R/use_metadata.R
+++ R/use_metadata.R
@@ -2,7 +2,7 @@
 #' 
 #' @description
 #' A metadata statement lists the owner of the dataset, how it was collected,
-#' and how it can be used (i.e. its licence). This function reads and
+#' and how it can be used (i.e. its' licence). This function reads and
 #' converts metadata saved in markdown (.md), Rmarkdown (.Rmd) or Quarto (.qmd) 
 #' to xml, and saves it in the `destination` folder.
 #' 
@@ -10,25 +10,33 @@
 #' [delma::write_eml()]. 
 #' @param source A metadata file in markdown (`.md`), Rmarkdown (`.Rmd`) or 
 #' Quarto markdown (`.qmd`). Defaults
-#' to `metadata.md`, which is the same as is created by [use_metdata_template()]
+#' to `metadata.md`, which is the same as is created by [use_metadata_template()]
 #' @param destination A file name to save the resulting `.xml` file. Defaults to 
-#' `data-publish/eml.xml`.
+#' `eml.xml`. Note that the file is always saved to the `data-publish` directory.
 #' @param overwrite By default, `use_metadata()` will not 
 #'   overwrite existing files. If you really want to do so, set this to `TRUE`. 
 #' @returns Does not return an object to the workspace; called for the side
-#' effect of building a file named `meta.xml` in the `data-publish` directory.
+#' effect of building a file in the `data-publish` directory.
 #' @seealso [use_metadata_statement()] to create a metadata statement template.
 #' @export
 use_metadata <- function(source = "metadata.Rmd", 
-                         destination = "data-publish/eml.xml",
+                         destination = "eml.xml",
                          overwrite = FALSE){
+  
+  # check file existence
   if(!file.exists(source)){
     cli::cli_abort("File {.file {source}} doesn't exist.")
   }
+  
   # import file, ensure EML metadata is added, convert to XML
   progress_update("Reading metadata statement...")
   metadata_tibble <- delma::read_md(source)
   
+  # get directory
+  use_directory("data-publish")
+  destination <- file.path("data-publish", destination)
+  
+  # set writing behaviour
   if(file.exists(destination)){
     if(overwrite){
       cli::cli_progress_step("Overwriting {.file {destination}}.")        
