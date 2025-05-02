633180f71f7ff3f39aef77d02fda97de3e554f3f
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2025-05-02 17:32:07 +1000

    adds another test (wip) for use_metadata()

--- tests/testthat/test-use_metadata.R
+++ tests/testthat/test-use_metadata.R
@@ -149,3 +149,18 @@ test_that("use_metadata() arguments work for Quarto Markdown", {
   unlink(root_directory)
   unlink("data-publish", recursive = TRUE)
 })
+
+# adding this at Dax's request
+test_that("use_metadata() works with no arguments", {
+  
+  current_wd <- here::here()
+  temp_dir <- withr::local_tempdir()
+  usethis::local_project(temp_dir, force = TRUE)
+  dir.create("data-publish")
+  write(readLines(paste0(current_wd, "/metadata.Rmd")), "metadata.Rmd")
+  
+  use_metadata()
+  expect_length(list.files("data-publish"), 1)
+  expect_in("eml.xml", list.files("data-publish"))
+  
+})
