458113eeeed8fe995adf42e3efdd643526c242e0
Dax Kellie <dax.kellie@csiro.au>
2025-05-08 17:49:08 +1000

    Add tests for `use_metadata()` and `use_schema()`

--- tests/testthat/test-use_metadata.R
+++ tests/testthat/test-use_metadata.R
@@ -33,25 +33,24 @@ test_that("use_metadata() fails when input doesn't exist", {
   unlink(temp_dir)
 })
 
-test_that("use_metadata() saves xml file using name provided by user", {
-  # set up
-  current_wd <- here::here()
-  temp_dir <- withr::local_tempdir()
-  usethis::local_project(temp_dir, force = TRUE)
-  use_metadata_template(quiet = TRUE) # add metadata.Rmd
-  
-  #tests
-  # setting a file name generates a file with that name, which is valid EML
-  use_metadata("metadata.Rmd", 
-               destination = "EXAMPLE.xml")
-  expect_true(
-    file.exists("data-publish/EXAMPLE.xml")
-    )
-  
-  # clean up
-  unlink("metadata.Rmd")
-  unlink(temp_dir)
-})
+# test_that("use_metadata() saves xml file using provided filename", {
+#   # set up
+#   current_wd <- here::here()
+#   temp_dir <- withr::local_tempdir()
+#   usethis::local_project(temp_dir, force = TRUE)
+#   use_metadata_template(quiet = TRUE) # add metadata.Rmd
+#   
+#   #tests
+#   use_metadata("metadata.Rmd", 
+#                destination = "EXAMPLE.xml")
+#   expect_true(
+#     file.exists("data-publish/EXAMPLE.xml")
+#     )
+#   
+#   # clean up
+#   unlink("metadata.Rmd")
+#   unlink(temp_dir)
+# })
 
 test_that("use_metadata() does not overwrite existing file by default", {
   # set up
@@ -93,80 +92,22 @@ test_that("use_metadata() overwrites file when overwrite = TRUE", {
   unlink(temp_dir)
 })
 
+test_that("use_metadata() reads quarto doc", {
+  # set up
+  current_wd <- here::here()
+  temp_dir <- withr::local_tempdir()
+  usethis::local_project(temp_dir, force = TRUE)
+  use_metadata_template("metadata.qmd", quiet = TRUE) # add metadata.qmd
+  
+  #tests
+  use_metadata("metadata.qmd", 
+               destination = "data-publish/EXAMPLE.xml")
+  expect_true(
+    file.exists("data-publish/EXAMPLE.xml")
+  )
+  
+  # clean up
+  unlink("metadata.qmd")
+  unlink(temp_dir)
+})
 
-# test_that("use_metadata() arguments work for Quarto Markdown", {
-#   use_metadata_template("EXAMPLE.Qmd",
-#                         overwrite = TRUE,
-#                         quiet = TRUE)
-# 
-#   # fails when `input` doesn't exist
-#   use_metadata("something.file") |>
-#     expect_error()
-# 
-#   # first check no file already present
-#   file.exists("data-publish") |> # no file at first
-#     expect_false()
-#   
-#   # now we have a problem, because `usethis` only puts information in the project directory
-#   root_path <- usethis::proj_path() |> 
-#     as.character() # where `usethis` places folders
-#   current_path <- getwd() # where `testthat` is working
-#   # these are likely to be different at all times, but hard to be sure
-#   # where they *are* different, we need to add `data-publish` here
-#   if(root_path != current_path){
-#     dir.create("data-publish")
-#     use_metadata("EXAMPLE.Qmd") |>
-#       expect_no_error()
-#     root_directory <- file.path(root_path, "data-publish")
-#     root_directory |>
-#       file.exists() |>
-#       expect_true()
-#   }else{
-#     # this is how tests should actually work
-#     use_metadata("EXAMPLE.Qmd") |>
-#       expect_no_error()
-#     file.exists("data-publish") |>
-#       expect_true()
-#   }
-# 
-#   # check file exists  
-#   file.exists("data-publish/eml.xml") |>
-#     expect_true()
-# 
-#   # setting a file name generates a file with that name, which is valid EML
-#   use_metadata("EXAMPLE.Qmd",
-#                destination = "EXAMPLE.xml")
-#   file.exists("data-publish/EXAMPLE.xml") |>
-#     expect_true()
-# 
-#   # where file already exists (as created in prev test), default is to not overwrite
-#   timestamp_1 <- file.info("data-publish/EXAMPLE.xml")$ctime
-#   use_metadata("EXAMPLE.Qmd",
-#                destination = "EXAMPLE.xml") |>
-#     expect_message()
-#   timestamp_2 <- file.info("data-publish/EXAMPLE.xml")$ctime
-#   expect_equal(timestamp_1, timestamp_2)
-# 
-#   # `overwrite` works, when requested
-#   timestamp_1 <- file.info("data-publish/EXAMPLE.xml")$ctime
-#   use_metadata("EXAMPLE.Qmd",
-#                destination = "EXAMPLE.xml",
-#                overwrite = TRUE) |>
-#     expect_message()
-#   timestamp_2 <- file.info("data-publish/EXAMPLE.xml")$ctime
-#   expect_true(timestamp_2 > timestamp_1)
-# 
-#   # check reimporting works
-#   x <- xml2::read_xml("data-publish/EXAMPLE.xml")
-#   inherits(x, c("xml_document", "xml_node")) |>
-#     expect_true()
-#   # ditto but to tibble
-#   delma::read_eml("data-publish/EXAMPLE.xml") |>
-#     expect_no_error()
-# 
-#   # clean up
-#   unlink("EXAMPLE.Qmd")
-#   unlink(root_directory)
-#   unlink("data-publish", recursive = TRUE)
-# })
-# 
---
+++ tests/testthat/test-use_schema.R
@@ -0,0 +1,42 @@
+
+test_that("use_schema() works with no arguments", {
+  # set up
+  current_wd <- here::here()
+  temp_dir <- withr::local_tempdir()
+  usethis::local_project(temp_dir, force = TRUE)
+  usethis::use_directory("data-publish")
+  df <- tibble::tibble(
+    decimalLatitude = c(44.4, 44.4)
+    ) |>
+    dplyr::mutate(
+      occurrenceID = random_id()
+      ) |>
+    write.csv("data-publish/occurrences.csv")
+  
+  #tests
+  use_schema() |>
+    expect_message()
+  expect_length(list.files("data-publish"), 2)
+  expect_in(c("meta.xml",
+              "occurrences.csv"), 
+            list.files("data-publish")
+            )
+  
+  # clean up
+  unlink("data-publish/occurrences.csv")
+  unlink(temp_dir)
+})
+
+test_that("use_schema() fails when no matching DwC csv files exist in directory", {
+  # set up
+  current_wd <- here::here()
+  temp_dir <- withr::local_tempdir()
+  usethis::local_project(temp_dir, force = TRUE)
+  
+  #tests
+  use_schema(quiet = TRUE) |>
+    expect_error("Must include")
+
+  # clean up
+  unlink(temp_dir)
+})
